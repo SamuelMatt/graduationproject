6402d53e6f004b45272e625bff8913e05d98e71c
Samuel <ms_achencer@yahoo.com>
2020-08-31 17:10:32 +0800

    blog article pag

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,23 +1,55 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50, verbose_name='分类名')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50, verbose_name='标签名')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=70)
-    body = models.TextField()
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
-    excerpt = models.CharField(max_length=200, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, verbose_name='标题')
+    body = models.TextField(verbose_name='正文')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    modified_time = models.DateTimeField(auto_now_add=True, verbose_name='修改时间')
+    excerpt = models.CharField(max_length=200, blank=True, verbose_name='摘要')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='分类')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='标签')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
+
+    class Meta:
+        verbose_name = "文章"
+        verbose_name_plural = verbose_name
+        # ordering = ["-created_time"]
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'id': self.id})
--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     url('', views.index, name='index'),
+    url(r'^post/(?P<id>[0-9]+)/$', views.detail, name='detail'),
 ]
