959615bcd07f32291f1a4b0276bfbb5cb537d248
Samuel <ms_achencer@yahoo.com>
2020-09-04 15:51:00 +0800

    fix base.html

--- apps/blog/templatetags/blog_extras.py
+++ apps/blog/templatetags/blog_extras.py
@@ -5,29 +5,29 @@ from ..models import Post, Category, Tag
 register = template.Library()
 
 
-@register.inclusion_tag('blog/inclusions/_recent_posts.html', takes_context=True)
-def show_recent_posts(context, num=5):
+@register.inclusion_tag('blog/inclusions/_recent_post.html', takes_context=True)
+def show_recent_post(context, num=5):
     return {
         'recent_post_list': Post.objects.all().order_by('-created_time')[:num],
     }
 
 
-@register.inclusion_tag('blog/inclusions/_archives.html', takes_context=True)
-def show_archives(context):
+@register.inclusion_tag('blog/inclusions/_archive.html', takes_context=True)
+def show_archive(context):
     return {
         'date_list': Post.objects.dates('created_time', 'month', order='DESC'),
     }
 
 
-@register.inclusion_tag('blog/inclusions/_categories.html', takes_context=True)
-def show_categories(context):
+@register.inclusion_tag('blog/inclusions/_categorie.html', takes_context=True)
+def show_categorie(context):
     return {
         'category_list': Category.objects.all(),
     }
 
 
-@register.inclusion_tag('blog/inclusions/_tags.html', takes_context=True)
-def show_tags(context):
+@register.inclusion_tag('blog/inclusions/_tag.html', takes_context=True)
+def show_tag(context):
     return {
         'tag_list': Tag.objects.all(),
     }
