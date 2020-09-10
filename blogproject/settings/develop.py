0bf8af4ce0b416290db78afe53bc0e7c84ce33e1
Samuel <ms_achencer@yahoo.com>
2020-09-10 17:03:24 +0800

    add mysql

--- blogproject/settings/develop.py
+++ blogproject/settings/develop.py
@@ -4,6 +4,24 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogproject',
+        'USER': '',
+        'PASSWORD': '*',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- blogproject/settings/product.py
+++ blogproject/settings/product.py
@@ -6,6 +6,26 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogproject',
+        'USER': '*',
+        'PASSWORD': '*',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'CONN_MAX_AGE': 60,
+        'OPTIONS': {'charset': 'utf8mb4'},
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
